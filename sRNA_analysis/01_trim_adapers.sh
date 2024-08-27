b5f06a8f3cf089fe9de112e7f846cabf9281425d
markram4 <75645855+markram4@users.noreply.github.com>
2024-08-27 14:44:35 -0500

    Update 01_trim_adapers.sh

--- sRNA_analysis/01_trim_adapers.sh
+++ sRNA_analysis/01_trim_adapers.sh
@@ -1,4 +1,7 @@
 ############################
 ## 03 Trim Illumina Universal adapter
 ############################
+cd /cluster/pixstor/slotkinr-lab/mkramer/projects/sRNA/R26/data/01_fastqFiles/
+ls *fq.gz | sed 's/.fq.gz//' > samples.txt
+
 sbatch /cluster/pixstor/slotkinr-lab/mkramer/projects/sRNA/R26/jobFiles/02_trim_adapters.array.sh
