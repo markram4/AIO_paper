624444f08da2dc05d01b4549ec89d14f3cc8e1ea
markram4 <75645855+markram4@users.noreply.github.com>
2024-08-27 14:52:47 -0500

    Update sRNA_processing.sh

--- sRNA_analysis/sRNA_processing.sh
+++ sRNA_analysis/sRNA_processing.sh
@@ -1,4 +1,12 @@
 ############################
+## 03 Trim Illumina Universal adapter
+############################
+cd /cluster/pixstor/slotkinr-lab/mkramer/projects/sRNA/R26/data/01_fastqFiles/
+ls *fq.gz | sed 's/.fq.gz//' > samples.txt
+
+sbatch /cluster/pixstor/slotkinr-lab/mkramer/projects/sRNA/R26/jobFiles/02_trim_adapters.array.sh
+
+############################
 ## 04 Map to genome, chloroplast and mitochondria and current transgenes
 ############################
 
