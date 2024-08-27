28eac28c057457945a14448b3f4744b6cd722de1
markram4 <75645855+markram4@users.noreply.github.com>
2024-08-27 14:52:25 -0500

    Update sRNA_processing.sh

--- sRNA_analysis/sRNA_processing.sh
+++ sRNA_analysis/sRNA_processing.sh
@@ -1,4 +1,24 @@
 ############################
+## 04 Map to genome, chloroplast and mitochondria and current transgenes
+############################
+
+
+#---------------- RUN COMMAND -------------------------
+sbatch /cluster/pixstor/slotkinr-lab/mkramer/projects/sRNA/R26/jobFiles/03_mapUnfiltered_reads.array.sh
+#------------------------------------------------------
+
+
+## The number of mapped reads from these files is used for downstream normalization, so make file with #mapped reads in each sample
+cd /cluster/pixstor/slotkinr-lab/mkramer/projects/sRNA/R26/data/04_mapUnfiltered
+echo "Sample" > tmpA.txt &&
+for file in *bam; do echo $(echo $file | sed 's/.unfiltered.mapped.bam//') >> tmpA.txt; done &
+
+echo "Num_Mapped_Reads" > tmpB.txt &&
+for file in *bam; do samtools view -c $file >> tmpB.txt; done &
+
+paste -d "\t" tmpA.txt tmpB.txt > number_mapped_reads.R26.txt
+
+############################
 ## 05 Filter for reads that don't map to chloroplast and mitochondria, and for size 18-28
 ############################
 mkdir /cluster/pixstor/slotkinr-lab/mkramer/projects/sRNA/R26/data/05_sizeFiltered
