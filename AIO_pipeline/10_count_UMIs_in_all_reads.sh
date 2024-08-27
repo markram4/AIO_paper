1fa24885d439785f1cdacf550328c2ec25c15bf0
markram4 <75645855+markram4@users.noreply.github.com>
2024-08-27 11:49:04 -0500

    Update and rename 0X_count_UMIs.sh to 10_count_UMIs_in_all_reads.sh

---
+++ AIO_pipeline/10_count_UMIs_in_all_reads.sh
@@ -0,0 +1,6 @@
+######################
+## Examine how many UMIs are present in data
+######################
+sbatch /cluster/pixstor/slotkinr-lab/mkramer/projects/target_capture/commonFiles/umi_analysis.sh ruby_round2 v2
+
+
