217b132ec9940a8caa43b6785d63a07d0572816b
markram4 <75645855+markram4@users.noreply.github.com>
2024-08-27 14:40:50 -0500

    Update 01_basecall_guppy.sh

--- AIO_pipeline/01_basecall_guppy.sh
+++ AIO_pipeline/01_basecall_guppy.sh
@@ -1,4 +1,8 @@
 ######################################
 ## 1 Base call raw Nanopore data using guppy basecaller
 ######################################
-
+/apps/src/ont-guppy_5.0.11/bin/guppy_basecaller -i /shares/slotkin/users/mkramer/ruby_round2/0_fast5/fc3/fc3 -s /scratch/TEMP_wkdir/TEMP_outf5_dir --device "cuda:all:100%" \
+  --flowcell FLO-MIN106 \
+  --kit SQK-LSK110 -q 0 --min_qscore 7 \
+  --recursive --fast5_out --chunks_per_runner 1024 \
+   --chunk_size 1024 --gpu_runners_per_device 1 --num_callers 1
