8bc1c80e42ccee99b562ccb5051ec9077632fd4e
markram4 <75645855+markram4@users.noreply.github.com>
2024-08-27 11:44:23 -0500

    Update generate_genome_mapping_scripts.sh

--- AIO_pipeline/commonFiles/generate_genome_mapping_scripts.sh
+++ AIO_pipeline/commonFiles/generate_genome_mapping_scripts.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-## get file names to map
-for file in /cluster/pixstor/slotkinr-lab/mkramer/projects/target_capture/ruby_round2/data/6_minimap/2_map_to_capture/output/non_targets_fastq/*fastq; do echo $(basename $file .fastq)| sed 's/.trimmed.3p.trimmed.5p//' >> /cluster/pixstor/slotkinr-lab/mkramer/projects/target_capture/ruby_round2/data/6_minimap/2_map_to_capture/output/non_targets_fastq/samples.txt; done 
-
-## Usage : 
-#bash /cluster/pixstor/slotkinr-lab/mkramer/projects/target_capture/commonFiles/generate_genome_mapping_scripts.sh\
-#ruby_round2\
-#/cluster/pixstor/slotkinr-lab/mkramer/projects/target_capture/ruby_round2/data/6_minimap/2_map_to_capture/output/non_targets_fastq/samples.txt
-
 ##---------------------------------------------------------------------------------
 
 ## FILENAMES
