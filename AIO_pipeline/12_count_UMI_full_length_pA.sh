e8a3b40c69580b21bcbb47710a1e42df5161b7d7
markram4 <75645855+markram4@users.noreply.github.com>
2024-08-27 11:49:54 -0500

    Create 12_count_UMI_full_length_pA.sh

---
+++ AIO_pipeline/12_count_UMI_full_length_pA.sh
@@ -0,0 +1,5 @@
+####################### 
+## Examine how many UMIs are present full length reads
+#######################
+
+sbatch /cluster/pixstor/slotkinr-lab/mkramer/projects/target_capture/commonFiles/umi_analysis.FL.sh ruby_round2 v2
